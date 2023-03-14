cf28295893d13b52f4105859a10e30752d89c500
sokolov-teach <92462062+sokolov-teach@users.noreply.github.com>
2023-03-14 08:29:18 +0600

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,37 @@
+document.write('\
+\
+        <hr>\
+        <ul class="social">\
+            <li class="social">\
+                <a class="social" href="https://www.youtube.com/@sortofschool">\
+                <img class="social" src="img/yt_logo.png">\
+                Youtube\
+            </a></li>\
+            <li class="social">\
+                <a class="social" href="https://patreon.com/sortofschool">\
+                <img class="social" src="img/pt_logo.png">\
+                Patreon\
+            </a></li>\
+            <li class="social">\
+                <a class="social" href="https://github.com/sokolov-teach">\
+                <img class="social" src="img/git_logo.png">\
+                Github\
+            </a></li>\
+            <li class="social">\
+                <a class="social" href="https://www.instagram.com/sortofschool/">\
+                <img class="social" src="img/ig_logo.png">\
+                Instagram\
+            </a></li>\
+            <li class="social">\
+                <a class="social" href="https://twitter.com/sortofschool">\
+                <img class="social" src="img/tw_logo.png">\
+                Twitter\
+            </a>\
+            <li class="social">\
+                <a class="social" href="mailto: sokolov.teach@gmail.com">\
+                <img class="social" src="img/email.png">\
+                E-mail\
+            </a></li>\
+        </ul>\
+\
+');
