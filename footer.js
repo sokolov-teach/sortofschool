e3de61cbfeedd2728e814a38ce0ef7ae2c758190
sokolov-teach <sokolov.teach@gmail.com>
2023-03-15 19:38:16 +0600

    folder restructure, equasion renderer
    - TeX to SVG and PNG equation renderer tool added
    - restructured some folders
    - changes in javascript header and footer to catch images from img folder with new folder structure

--- footer.js
+++ footer.js
@@ -1,37 +1,52 @@
+// Get the current path of the HTML page
+var currentPath = window.location.pathname;
+
+// Extract the current folder from the path
+var currentFolder = currentPath.substring(0, currentPath.lastIndexOf('/') + 1);
+
+// Determine the correct image folder based on the current folder
+// If the current folder is the root directory, use 'img/'
+// Otherwise, use '../img/'
+var imgFolder = currentFolder === '/' ? 'img/' : '../img/';
+
 document.write('\
-\
-        <hr>\
-        <ul class="social">\
-            <li class="social">\
-                <a class="social" href="https://www.youtube.com/@sortofschool">\
-                <img class="social" src="img/youtube_logo.png">\
+    <hr>\
+    <ul class="social">\
+        <li class="social">\
+            <a class="social" href="https://www.youtube.com/@sortofschool">\
+                <img class="social" src="' + imgFolder + 'youtube_logo.png">\
                 Youtube\
-            </a></li>\
-            <li class="social">\
-                <a class="social" href="https://patreon.com/sortofschool">\
-                <img class="social" src="img/pt_logo.png">\
+            </a>\
+        </li>\
+        <li class="social">\
+            <a class="social" href="https://patreon.com/sortofschool">\
+                <img class="social" src="' + imgFolder + 'pt_logo.png">\
                 Patreon\
-            </a></li>\
-            <li class="social">\
-                <a class="social" href="https://github.com/sokolov-teach">\
-                <img class="social" src="img/git_logo.png">\
+            </a>\
+        </li>\
+        <li class="social">\
+            <a class="social" href="https://github.com/sokolov-teach">\
+                <img class="social" src="' + imgFolder + 'git_logo.png">\
                 Github\
-            </a></li>\
-            <li class="social">\
-                <a class="social" href="https://www.instagram.com/sortofschool/">\
-                <img class="social" src="img/ig_logo.png">\
+            </a>\
+        </li>\
+        <li class="social">\
+            <a class="social" href="https://www.instagram.com/sortofschool/">\
+                <img class="social" src="' + imgFolder + 'ig_logo.png">\
                 Instagram\
-            </a></li>\
-            <li class="social">\
-                <a class="social" href="https://twitter.com/sortofschool">\
-                <img class="social" src="img/tw_logo.png">\
+            </a>\
+        </li>\
+        <li class="social">\
+            <a class="social" href="https://twitter.com/sortofschool">\
+                <img class="social" src="' + imgFolder + 'tw_logo.png">\
                 Twitter\
             </a>\
-            <li class="social">\
-                <a class="social" href="mailto: sokolov.teach@gmail.com">\
-                <img class="social" src="img/email.png">\
+        </li>\
+        <li class="social">\
+            <a class="social" href="mailto: sokolov.teach@gmail.com">\
+                <img class="social" src="' + imgFolder + 'email.png">\
                 E-mail\
-            </a></li>\
-        </ul>\
-\
+            </a>\
+        </li>\
+    </ul>\
 ');
--- header.js
+++ header.js
@@ -1,41 +1,57 @@
+// Get the current path of the HTML page
+var currentPath = window.location.pathname;
+
+// Extract the current folder from the path
+var currentFolder = currentPath.substring(0, currentPath.lastIndexOf('/') + 1);
+
+// Determine the correct image folder based on the current folder
+// If the current folder is the root directory, use 'img/'
+// Otherwise, use '../img/'
+var imgFolder = currentFolder === '/' ? 'img/' : '../img/';
+
 document.write('\
-\
     <nav class="header">\
-            <a class="header" href="/">Sort of School</a>\
-            <a class="header" href="/donate">Donate</a>\
-        </nav>\
-        <ul class="social">\
-            <li class="social">\
-                <a class="social" href="https://www.youtube.com/@sortofschool">\
-                <img class="social" src="img/youtube_logo.png">\
+        <a class="header" href="/">Sort of School</a>\
+        <a class="header" href="/donate">Donate</a>\
+    </nav>\
+    <ul class="social">\
+        <li class="social">\
+            <a class="social" href="https://www.youtube.com/@sortofschool">\
+                <img class="social" src="' + imgFolder + 'youtube_logo.png">\
                 Youtube\
-            </a></li>\
-            <li class="social">\
-                <a class="social" href="https://patreon.com/sortofschool">\
-                <img class="social" src="img/pt_logo.png">\
+            </a>\
+        </li>\
+        <li class="social">\
+            <a class="social" href="https://patreon.com/sortofschool">\
+                <img class="social" src="' + imgFolder + 'pt_logo.png">\
                 Patreon\
-            </a></li>\
-            <li class="social">\
-                <a class="social" href="https://github.com/sokolov-teach">\
-                <img class="social" src="img/git_logo.png">\
+            </a>\
+        </li>\
+        <li class="social">\
+            <a class="social" href="https://github.com/sokolov-teach">\
+                <img class="social" src="' + imgFolder + 'git_logo.png">\
                 Github\
-            </a></li>\
-            <li class="social">\
-                <a class="social" href="https://www.instagram.com/sortofschool/">\
-                <img class="social" src="img/ig_logo.png">\
+            </a>\
+        </li>\
+        <li class="social">\
+            <a class="social" href="https://www.instagram.com/sortofschool/">\
+                <img class="social" src="' + imgFolder + 'ig_logo.png">\
                 Instagram\
-            </a></li>\
-            <li class="social">\
-                <a class="social" href="https://twitter.com/sortofschool">\
-                <img class="social" src="img/tw_logo.png">\
+            </a>\
+        </li>\
+        <li class="social">\
+            <a class="social" href="https://twitter.com/sortofschool">\
+                <img class="social" src="' + imgFolder + 'tw_logo.png">\
                 Twitter\
             </a>\
-            <li class="social">\
-                <a class="social" href="mailto: sokolov.teach@gmail.com">\
-                <img class="social" src="img/email.png">\
+        </li>\
+        <li class="social">\
+            <a class="social" href="mailto: sokolov.teach@gmail.com">\
+                <img class="social" src="' + imgFolder + 'email.png">\
                 E-mail\
-            </a></li>\
-        </ul>\
-        <hr>\
-\
+            </a>\
+        </li>\
+    </ul>\
+    <hr>\
 ');
+
